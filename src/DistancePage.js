1c7b21b884d3ab79214ad767cc6d95e040953922
Rachelle Bosman <32331937+rachellebosman@users.noreply.github.com>
2021-05-24 08:03:02 +0200

    first commit

---
+++ src/DistancePage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import DistanceBar from './components/DistanceBar'
+
+function distanceLeft() {
+
+ 
+
+    return (
+        <div>
+            <p> Distance Left </p>
+            <DistanceBar /> 
+            
+        </div>
+    )
+}
+
+export default distanceLeft
---
+++ src/components/DistanceBar.js
@@ -0,0 +1,15 @@
+import React, {useState, setState} from 'react'
+import *  as d3 from 'd3';
+
+function DistanceBar() {
+   
+
+
+    return (
+        <div className="x">
+            <p> dit wordt de progrss barr </p>
+        </div>
+    )
+}
+
+export default DistanceBar
