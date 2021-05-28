550da626f5e1a395d925887a7ef77c8f1a05c37e
Thomas Machielsen <tmachielsen@mirabeau.nl>
2021-05-28 11:02:20 +0200

    chores(App): add comments for App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+// je importeert 2 keer een style bestand, kan dat anders?
 import './App.scss';
 import './styles/style.scss';
 import MainPage from './pages/MainPage';
 
 function App() {
   return (
+    // classNames zijn niet consistent
     <div className="App">
+      {/* hebben we het over gehad */}
       <header className="App-header">
 
         <MainPage />
+        {/*  dit soort spaces kunnen afgevangen worden door linting */}
 
 
       </header>
