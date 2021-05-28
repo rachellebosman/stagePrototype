2fb4e5058c3acaf34cce10a4b1d6f98747bed523
Thomas Machielsen <tmachielsen@mirabeau.nl>
2021-05-28 10:56:26 +0200

    chores(fecthData): add comments for fetchData

--- src/components/fetchData.js
+++ src/components/fetchData.js
@@ -1,10 +1,15 @@
+// wanneer is iets een component?
+// bij code wil je graag documentatie, zoek eens op jsDocs
 export const fetchData = (url) => {
 
-    return fetch(url)
-    .then(res => res.json())
-    //.then(data => console.log(data))
-    .then(data => data) 
-    .catch(error => console.log('error', error))
+    return (
+      fetch(url)
+        .then((res) => res.json())
+        //.then(data => console.log(data))
+        .then((data) => data)
+        // onbtbreekt error handling https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#checking_that_the_fetch_was_successful
+        .catch((error) => console.log("error", error))
+    );
 
 }
 
